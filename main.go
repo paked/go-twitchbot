eb25f2a27a6f8ac7831181ebc3c8003eb4e2d30d
Harrison <harrison@theshoebridges.com>
2014-09-01 13:26:52 +1000

    Split IRC bot from viewercount.

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	ircevent "github.com/thoj/go-ircevent"
+	"log"
+)
+
+var stream = flag.String("stream", "tehhcwool", "Your (or someone elses) streamname")
+var clientID = flag.String("clientID", "9po4ts2jz2niigqq3o9gtt2ntw69njf", "Your client ID (set in settings/connections)")
+
+var ircPassword = flag.String("password", "oauth:c3v70dw7nvznz2amu2wi0ai177cb7sf", "Twitch oauth password, get yours at http://twitchapps.com/tmi")
+
+func main() {
+	flag.Parse()
+
+	irc := ircevent.IRC(*stream, *stream)
+
+	irc.Password = *ircPassword
+
+	err := irc.Connect("irc.twitch.tv:6667")
+
+	if err != nil {
+		log.Println("IRC CON FAILED")
+		return
+	}
+
+	irc.AddCallback("001",
+		func(e *ircevent.Event) {
+			irc.Join(*stream)
+		})
+
+	irc.AddCallback("JOIN",
+		func(e *ircevent.Event) {
+			log.Println("Connected: " + e.Raw)
+			irc.Privmsg("#"+*stream, "go away")
+		})
+
+	irc.AddCallback("PRVMSG",
+		func(e *ircevent.Event) {
+			log.Println("[ " + e.Nick + " ] " + e.Message())
+		})
+
+	irc.Loop()
+}
