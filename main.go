38c6d69a5605e5887def08712367a798ca0d1539
Harrison <harrison@theshoebridges.com>
2014-09-02 13:02:39 +1000

    Made shit work

--- main.go
+++ main.go
@@ -11,35 +11,37 @@ var clientID = flag.String("clientID", "9po4ts2jz2niigqq3o9gtt2ntw69njf", "Your 
 
 var ircPassword = flag.String("password", "oauth:c3v70dw7nvznz2amu2wi0ai177cb7sf", "Twitch oauth password, get yours at http://twitchapps.com/tmi")
 
+var server = flag.String("ip", "irc.freenode.net:6667", "IP")
+
+var room = "#lolok"
+
 func main() {
-	flag.Parse()
-
+	log.Println("Starting up, connecting to ", *server, " as ", *stream)
+	// Connect to IRC server
 	irc := ircevent.IRC(*stream, *stream)
-	irc.Debug = true
-	irc.Password = *ircPassword
 
-	err := irc.Connect("irc.twitch.tv:6667")
+	err := irc.Connect(*server)
 
 	if err != nil {
-		log.Println("IRC CON FAILED")
-		return
+		log.Fatalln("Could not connect to server")
 	}
+	defer irc.Disconnect()
 
-	irc.AddCallback("001",
-		func(e *ircevent.Event) {
-			irc.Join(*stream)
-		})
+	// When we've connected to the IRC server, go join the room!
+	log.Println("Connected: ", *stream)
+	irc.AddCallback("001", func(e *ircevent.Event) {
+		irc.Join(room)
+		log.Println("Joined room ", room)
+	})
 
-	irc.AddCallback("JOIN",
-		func(e *ircevent.Event) {
-			log.Println("Connected: " + e.Raw)
-			irc.Privmsg("#tehhcwool", "Hello")
-		})
-
-	irc.AddCallback("GLOBALMSG",
-		func(e *ircevent.Event) {
-			log.Println("[ " + e.Nick + " ] " + e.Message())
-		})
+	// Say something on arrival
+	irc.AddCallback("JOIN", func(e *ircevent.Event) {
+		irc.Privmsg(room, "LDLC? NiP")
+	})
+	// Check each message to see if it contains a URL, and return the title
+	irc.AddCallback("PRIVMSG", func(e *ircevent.Event) {
+		log.Printf("[%v] %v", e.Nick, e.Message())
+	})
 
 	irc.Loop()
 }
