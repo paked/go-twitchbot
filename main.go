614f246656f0b26203cd71cbed16d124a0fa0066
Harrison <harrison@theshoebridges.com>
2014-09-02 21:51:23 +1000

    Finally done and finished

--- main.go
+++ main.go
@@ -6,38 +6,39 @@ import (
 	"log"
 )
 
-var stream = flag.String("stream", "tehhcwool", "Your (or someone elses) streamname")
+var stream = flag.String("stream", "paked", "Your (or someone elses) streamname")
 var clientID = flag.String("clientID", "9po4ts2jz2niigqq3o9gtt2ntw69njf", "Your client ID (set in settings/connections)")
 
-var ircPassword = flag.String("password", "oauth:c3v70dw7nvznz2amu2wi0ai177cb7sf", "Twitch oauth password, get yours at http://twitchapps.com/tmi")
-
-var server = flag.String("ip", "irc.freenode.net:6667", "IP")
-
-var room = "#lolok"
+var ircPassword = flag.String("password", "oauth:jerbrv285soc70ckkhr2qdqgal4bul1", "Twitch oauth password, get yours at http://twitchapps.com/tmi")
+var room = flag.String("chatroom", "", "The chat you want to listen in on")
+var server = flag.String("ip", "irc.twitch.tv:6667", "IP")
 
 func main() {
-	log.Println("Starting up, connecting to ", *server, " as ", *stream)
+	log.Println("Starting up, connecting to ", server, " as ", *stream)
+
 	// Connect to IRC server
 	irc := ircevent.IRC(*stream, *stream)
-
+	irc.Password = *ircPassword
 	err := irc.Connect(*server)
 
 	if err != nil {
 		log.Fatalln("Could not connect to server")
 	}
-	defer irc.Disconnect()
+
+	if *room == "" {
+		*room = "#" + *stream
+	}
 
 	// When we've connected to the IRC server, go join the room!
 	log.Println("Connected: ", *stream)
 	irc.AddCallback("001", func(e *ircevent.Event) {
-		irc.Join(room)
-		log.Println("Joined room ", room)
+		irc.Join(*room)
 	})
 
-	// Say something on arrival
 	irc.AddCallback("JOIN", func(e *ircevent.Event) {
-		irc.Privmsg(room, "LDLC? NiP")
+		log.Println("[{ME}] Listening to your chat")
 	})
+
 	// Check each message to see if it contains a URL, and return the title
 	irc.AddCallback("PRIVMSG", func(e *ircevent.Event) {
 		log.Printf("[%v] %v", e.Nick, e.Message())
